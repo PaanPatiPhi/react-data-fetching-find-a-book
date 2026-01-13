57fd9be4659b832dbf6868b28f643f4fcb91bc4c
phi <patiparn.phi@gmail.com>
2026-01-13 16:09:20 +0700

    feat: Added search input and debouncing logic to fetch books from Google Books API - Displayed list of books based on user search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [books, setBooks] = useState([]);
+  const [search, setSearch] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+
+  async function getData() {
+    let response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q="${search}"`
+    );
+    console.log(response.data.items);
+    setBooks(response.data.items);
+    console.log(search)
+  }
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setDebouncedSearch(search);
+    },500);
+    return ()=> clearTimeout(timer);
+  },[search])
+  useEffect(() => {
+    if (debouncedSearch === "") {
+      return;
+    }
+    getData(debouncedSearch);
+  }, [debouncedSearch]);
+
+  return (
+    <div className="App">
+      <h2>Book List</h2>
+      <input
+        type="text"
+        placeholder="ค้นหาหนังสือ ..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <hr />
+      <ul>
+        {Array.isArray(books) &&
+        books.map((item) => (
+          <li key={item.id}>{item.volumeInfo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
